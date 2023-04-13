ffe0dc24956ae7879ead4b91a670ea18200c56b7
thurendous <thurendous2@gmail.com>
2023-04-13 15:50:19 +0900

    test

--- src/Constant.sol
+++ src/Constant.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.13;
 
 contract Constant {
     address public constant varConstant = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
-    address public immutable varImmutable;
+    address public immutable varImmutable = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
     address public variable = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
 
     constructor () {
-        varImmutable = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
+        
     }
 }
